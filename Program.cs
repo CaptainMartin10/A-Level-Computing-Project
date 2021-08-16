2ea77702d23d5d43390f8bfb86465fa08f52df63
Ryan Midgley <CptnMrtn10@gmail.com>
2021-08-16 14:41:16 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace A_Level_Computing_Project
+{
+    public static class Program
+    {
+        [STAThread]
+        static void Main()
+        {
+            using (var game = new Game1())
+                game.Run();
+        }
+    }
+}
