1ee4fe87e090cd1e1af7932d418776e9ee9046f9
Ryan MIDGLEY <CptnMrtn10@gmail.com>
2021-09-27 12:19:24 +0100

    Still needs structures in text file

--- Army.cs
+++ Army.cs
@@ -6,16 +6,14 @@ namespace A_Level_Computing_Project
 {
     public class Army
     {
-        public int X, Y, Infantry, Archers, Cavalry;
+        public int X, Y, Infantry;
         public Country OwnedBy;
 
-        public Army(int x, int y, int i, int a, int c, Country o)
+        public Army(int x, int y, int i, Country o)
         {
             X = x;
             Y = y;
             Infantry = i;
-            Archers = a;
-            Cavalry = c;
             OwnedBy = o;
         }
     }
