0ea15d19bc07ceefe914e19f7e8cdb1a000cc9d4
Ryan MIDGLEY <CptnMrtn10@gmail.com>
2021-10-21 14:09:22 +0100

    Needs Army Movement

---
+++ PhantomArmy.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace A_Level_Computing_Project
+{
+    class PhantomArmy
+    {
+        public int X, Y;
+
+        public PhantomArmy(int x, int y)
+        {
+            X = x;
+            Y = y;
+        }
+    }
+}
